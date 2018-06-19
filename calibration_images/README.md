f7e322b2e9e35e27756d9bd62f654606e69193bf
nico <nicozhang@slightech.com>
2018-06-19 17:38:30 +0800

    add fish_eye calibration

---
+++ calibration_images/README.md
@@ -0,0 +1,25 @@
+## calibrate camera parameters 
+1\. launch `mynt_eye_ros_wrapper`
+```
+cd MYNT-EYE-SDK-2	
+make ros
+source wrappers/ros/devel/setup.bash	
+roslaunch mynt_eye_ros_wrapper mynteye.launch
+```
+2\. launch `calibration_images`
+
+`calibration_images pkg` is used to get the calibration image list, press the `w`key to save image to `mynt_images file. We collect about 30 images to calibrate the camera parameters. here is [chessbord_9_6](./chessbord_9*6.jpg), grid size=25.16 mm.
+
+
+```
+roslaunch calibration_images calibration_images.launch 
+```
+
+3\. run `camera_model` node to extrat camera parameters **in mynt_images**, copy the contents to `<vins>/config/mynteye/mynteye_config.yaml`.
+
+```
+roscd vins_estimator/../calibration_images/mynt_images
+rosrun camera_model Calibration -w 9 -h 6 -s 25.16 -p fisheye_ -e .jpg -i . --camera-model mei
+```
+	
+The `camera_camera_calib.yaml` file is generated in the **mynt_images** after the calibration is completed.
---
+++ calibration_images/src/calibration_images.cpp
@@ -0,0 +1,41 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include "cv.h"
+#include "highgui.h"
+#include <sstream>
+#include <cstdlib>
+#include <string>
+
+static int i = 1;
+static std::string save_image_path = "save_image_path_error";
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+     cv::namedWindow("img");
+     cv::imshow("img", cv_bridge::toCvShare(msg, "8UC1")->image);
+     char key =    cv::waitKey(100);
+     std::string s = save_image_path + "/fisheye_";
+     std::ostringstream oss;
+     oss << s << i << ".jpg";
+     if(key == 'w')
+     {
+        cv::imwrite(oss.str(),cv_bridge::toCvShare(msg, "8UC1")->image);
+        i++;
+     }
+
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "calibration_images");
+  ros::NodeHandle nh("~");
+  std::string image_topic("topic_error");
+  nh.getParam("image_topic", image_topic);
+  nh.getParam("save_image_path", save_image_path);
+  ROS_INFO_STREAM("image_topic: " << image_topic);
+  ROS_INFO_STREAM("save_image_path: " << save_image_path);
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe(image_topic, 1, imageCallback);
+  ros::spin();
+}
